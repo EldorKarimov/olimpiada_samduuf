bfa5dda7cf3c5858ca919233c792d85872d4848a
EldorKarimov <hacktivist19021@gmail.com>
2023-12-18 17:14:48 +0500

    added frontend templates

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+
+class Direction(models.Model):
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
+
+class CustomUser(AbstractUser):
+    full_name = models.CharField(max_length = 255)
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text=(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        error_messages={
+            "unique": ("A user with that username already exists."),
+        },
+    )
+    phone = models.CharField(max_length = 13)
+    direction = models.ForeignKey(Direction, on_delete = models.PROTECT, null = True)
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["full_name", "phone"]
+
+    def save(self, *args, **kwargs):
+        if not self.password.startswith('pbkdf2_sha256'):
+            self.password = make_password(self.password)
+
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.full_name
--- config/settings.py
+++ config/settings.py
@@ -1,27 +1,20 @@
-import environ
-import os
 from pathlib import Path
-
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -43,7 +36,7 @@ INSTALLED_APPS = [
     'quiz',
 
     # packages
-    'widget-tweaks',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -123,8 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+# STATIC_ROOT = 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
