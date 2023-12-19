283e75c8232ca95f340a7274ef1210686dcf410a
Eldor Karimov <hacktivist19021@gmail.com>
2023-12-19 17:07:53 +0500

    added quiz model

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import QuizModel, Question, Answer, Result
 
-# Register your models here.
+@admin.register(QuizModel)
+class QuizModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {"slug":['name']}
+
+class AnswerInlineModel(admin.TabularInline):
+    model = Answer
+    fields = ['answer_name', 'is_right']
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question_name']
+    fields = ['question_name', 'quiz']
+    inlines = [AnswerInlineModel]
+
+
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['question_count', 'correct_question_count', 'user', 'quiz', 'created']
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class QuizModel(BaseModel):
+    name = models.CharField(max_length=150)
+    author = models.CharField(max_length = 120)
+    slug = models.SlugField(max_length=150, unique=True)
+    image = models.ImageField(upload_to='media/images')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Quizzes"
+
+class Question(BaseModel):
+    question_name = models.CharField(max_length=255)
+    quiz = models.ForeignKey(QuizModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.question_name
+    
+class Answer(BaseModel):
+    answer_name = models.CharField(max_length=255)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_right = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.answer_name
+    
+class Result(BaseModel):
+    question_count = models.PositiveIntegerField()
+    correct_question_count = models.PositiveIntegerField()
+    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)  # null=True ni qo'shing
+    quiz = models.ForeignKey(QuizModel, on_delete=models.SET_NULL, null=True)  # null=True ni qo'shing
+
+    def __str__(self):
+        return self.user.full_name if self.user else "Anonymous User"
